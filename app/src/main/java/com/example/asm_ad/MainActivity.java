d0fe3b5532a8bac40c25def0dec03d4323fb3ce6
tuanct413 <tuanct413@gmail.com>
2025-03-13 18:27:56 +0700

    first tuan 3

--- app/src/main/java/com/example/asm_ad/MainActivity.java
+++ app/src/main/java/com/example/asm_ad/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.asm_ad;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,38 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editEmail ;
+    private EditText editPassword ;
+    private Button btnLogin ;
+    private TextView txtthongbao ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        editEmail = findViewById(R.id.editEmail);
+        editPassword = findViewById(R.id.editPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        txtthongbao = findViewById(R.id.txtthongbao);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               if(editEmail.getText().toString().equals("admin")
+                       && editPassword.getText().toString().equals("admin")) {
+                   navigateToRegister();
+                   txtthongbao.setText("Đăng nhập thành công");
+               }else{
+                   txtthongbao.setText("Đăng nhập thất bại");
+               }
+            }
         });
+
+    }
+    private void navigateToRegister(){
+        Intent intent = new Intent(MainActivity.this,Home.class);
+        startActivity(intent);
+        finish();
     }
 }
